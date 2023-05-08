8135f27ad0555a566ea7d1a4809658cdac5dd0d9
FelixNotter <shepherdeinstein@gmail.com>
2023-05-08 15:05:31 +0000

    Update working

--- working.py
+++ working.py
@@ -8,8 +8,10 @@ inventory = {
         "price":"2.99",
         "brand":"Nido",
     }
-}
 
+   
+}
+# New App
 @app.get("/get-item/{item_id}")
 def get_item(item_id:int):
     return inventory[item_id]
